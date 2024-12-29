b728ddb2e912855e71131d8452d1b6466b13e7a6
realJanWeiss <171038268+realJanWeiss@users.noreply.github.com>
2024-12-29 10:47:50 +0100

    backend: update dependencies and improve eslint rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,6 +17,12 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        caughtErrors: 'none'
+      }
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
