ae637c0edb7d6f7d339ed67b512074319d8d41f8
realJanWeiss <171038268+realJanWeiss@users.noreply.github.com>
2025-01-09 16:13:50 +0100

    add image on welcome page

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# Geschmeckt Frontend
+
+## Attribution
+
+Thank you to [OpenMoji](https://openmoji.org) which is an open-source emoji and icon project. License: CC BY-SA 4.0
--- frontend/src/app/welcome/welcome.page.ts
+++ frontend/src/app/welcome/welcome.page.ts
@@ -5,17 +5,28 @@ import {
   IonContent,
   IonButton,
   IonItemDivider,
+  IonImg,
 } from '@ionic/angular/standalone';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ModalComponent } from '../ui-library/modal/modal.component';
 
+const possibleImages = [
+  'beverage-box',
+  'chocolate',
+  'cookie',
+  'sandwich',
+  'spaghetti',
+  'stuffed-flatbread',
+  'taco',
+] as const;
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
   imports: [
+    IonImg,
     IonButton,
     IonItemDivider,
     IonContent,
@@ -28,8 +39,12 @@ import { ModalComponent } from '../ui-library/modal/modal.component';
 })
 export class WelcomePage {
   isLoggingIn = false;
+  randomImage: string;
 
-  constructor(private readonly cdr: ChangeDetectorRef) {}
+  constructor(private readonly cdr: ChangeDetectorRef) {
+    const randomIndex = Math.floor(Math.random() * possibleImages.length);
+    this.randomImage = `/assets/${possibleImages[randomIndex]}.svg`;
+  }
 
   closeLoginModal() {
     this.isLoggingIn = false;
